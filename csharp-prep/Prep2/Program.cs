f7f2c95deaeb957c79520e45094a6a7d1950bf70
Fernando <fernandochaza@gmail.com>
2023-04-17 12:31:49 -0300

    Finish prep2 with stetch challenge

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,60 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Prep2 Activity");
+
+        string letterGrade;
+        string sign = "";
+
+        Console.Write("Enter your grade percentage: ");
+        string valueFromUser = Console.ReadLine();
+
+        int percentageGrade = int.Parse(valueFromUser);
+
+        int reminder = percentageGrade % 10;
+
+        if (percentageGrade > 60 && percentageGrade < 93)
+        {
+            if (reminder >= 7)
+            {
+                sign = "+";
+            }
+            else if (reminder < 3)
+            {
+                sign = "-";
+            }
+        }
+
+        if (percentageGrade >= 70)
+        {
+            if (percentageGrade < 80)
+            {
+                letterGrade = "C" + sign;
+            }
+            else if (percentageGrade < 90)
+            {
+                letterGrade = "B" + sign;
+            }
+            else
+            {
+                letterGrade = "A" + sign;
+            }
+            Console.WriteLine($"Congratulations! You have passed the course with a letter grade of {letterGrade}!");
+        }
+        else 
+        {
+            if (percentageGrade > 59)
+            {
+                letterGrade = "D" + sign;
+            }
+            else
+            {
+                letterGrade = "F";
+            }
+        Console.WriteLine($"Your letter grade is {letterGrade}.");
+        Console.WriteLine("You need a C to pass this course. Try again next time!");
+        }
     }
+
+
 }
