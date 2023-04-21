83e974dc7114dd478808ccfb93bb42198911fa4c
Fernando <fernandochaza@gmail.com>
2023-04-20 22:55:59 -0300

    Finished stretch challenge 1

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -6,12 +6,15 @@ class Program
     {
 
         string result;
+        int guessAttempt = 0;
 
         Random randomGenerator = new Random();
-        int magicNumber = randomGenerator.Next(1, 100);
+        int magicNumber = randomGenerator.Next(1, 101);
         
         do
-        {        
+        {
+            guessAttempt = guessAttempt + 1;
+            
             Console.Write("What is your guess? ");
             string getGuessValue = Console.ReadLine();
             int guessValue = int.Parse(getGuessValue);
@@ -31,6 +34,8 @@ class Program
 
             Console.WriteLine($"{result}");
         } while (result != "You guessed it!");
+
+        Console.WriteLine($"Attempts made: {guessAttempt}");
 
     }
 }
