5c46a5e2a0b2c9c8499521c2990d876c7e87f472
Fernando <fernandochaza@gmail.com>
2023-05-04 16:33:51 -0300

    feat: Create Journal class structure
    Create journal class with attributes and methods defined in the diagram

---
+++ prove/Develop02/Journal.cs
@@ -0,0 +1,42 @@
+// Responsibilities: 
+// - Store the journal's entries
+// - Save the journal in a txt file
+// - Load a stored txt file containing Journal entries
+// - Display the journal entries to the user
+using System.IO;
+public class Journal
+{
+    public string _name;
+    public List<Entry> = new List<Entry>();
+
+    public string LoadJournalFromFile()
+    {
+        return "";
+    }
+
+    public void SaveJournalToFile()
+    {
+
+    }
+
+    public void DisplayJournal()
+    {
+        
+    }
+
+
+    // {
+    //     string filename;
+    //     Console.Write("Enter the name of the file: ");
+    //     filename = Console.ReadLine();
+
+    //     string[] lines = File.ReadAllLines(filename);
+
+    //     return "";
+
+    // }
+
+        // string filename;
+        // using (StreamWriter outputFile = new StreamWriter(filename))
+        // outputFile.
+}
