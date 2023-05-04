0e339adbf5998a38b7656540325233447955575a
Fernando <fernandochaza@gmail.com>
2023-05-04 16:50:04 -0300

    feat: Create Prompts class structure
    Create Prompt class structure as defined in its diagram

---
+++ prove/Develop02/Prompts.cs
@@ -0,0 +1,13 @@
+// Responsibilities:
+// - Generate a random prompt
+
+public class Prompts
+{
+    public string[] _promptList;
+    public string _prompt;
+
+    public string showRandomPrompt()
+    {
+        return "";
+    }
+}
