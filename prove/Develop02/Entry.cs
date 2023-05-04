ef021427d0075fcf6bf9e41f7ebfe98271eac90d
Fernando <fernandochaza@gmail.com>
2023-05-04 16:45:35 -0300

    feat: Create Entry class structure
    Create Entry class with the attributes and methods defined in its
    diagram

---
+++ prove/Develop02/Entry.cs
@@ -0,0 +1,23 @@
+// Responsibilities:
+// - Display a random prompt to the user
+// - Get the user answer and current date
+// - Create an entry with the prompt, answer and date
+// - Display an entry (prompt, answer, date)
+
+public class Entry
+{
+    public string _prompt;
+    public string _answer;
+    public string _date;
+
+    public string CreateEntry()
+    {
+        return "";
+    }
+
+    public void DisplayEntry()
+    {
+
+    }
+
+}
