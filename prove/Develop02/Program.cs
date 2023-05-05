b38d59f6526adf8f0521e1b50849522db201187a
Fernando <fernandochaza@gmail.com>
2023-05-04 23:36:43 -0300

    feat: Generate new Journal and Menu in Main
    Generate a new instance of Journal and Menu
    using the user name for the header and a list of options for the menu

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,26 @@
 using System;
 
-class Program
+public class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        // Instantiate a new Journal
+        Journal journal = new Journal();
+        journal._name = "John";
+
+        // Define a header
+        string header = $"Hello {journal._name}! Welcome to your Journal Manager";
+
+        // Create the menu options
+        List<string> options = new List<string>();
+        options.Add("Create a new entry");
+        options.Add("Display my journal");
+        options.Add("Save today's entries");
+        options.Add("Load my journal");
+        options.Add("Quit");
+
+        // Instantiate a new Menu and Display the menu
+        Menu mainMenu = new Menu(header, options);
+        mainMenu.DisplayMenu();
     }
 }
