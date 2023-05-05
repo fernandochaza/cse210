5e10325e849769be80203fde65af10bf5b8cc047
Fernando <fernandochaza@gmail.com>
2023-05-05 00:54:34 -0300

    feat: Add a loop to display options
    Add a loop to display options until user chose option 5
    Add a switch to manage new entries case

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,9 @@ public class Program
 {
     static void Main(string[] args)
     {
+
+        string userChoice;
+
         // Instantiate a new Journal
         Journal journal = new Journal();
         journal._name = "John";
@@ -19,8 +22,25 @@ public class Program
         options.Add("Load my journal");
         options.Add("Quit");
 
-        // Instantiate a new Menu and Display the menu
+        // Instantiate a new Menu
         Menu mainMenu = new Menu(header, options);
-        mainMenu.DisplayMenu();
+        mainMenu.DisplayHeader();
+
+        do
+        {
+            // Display the menu options
+            mainMenu.DisplayOptions();
+
+            userChoice = Console.ReadLine();
+
+            switch (userChoice)
+            {
+                case "1":
+                    Entry newEntry = new Entry();
+                    journal._entries.Add(newEntry);
+                    break;
+            }
+        } while (userChoice != "5");
+
     }
 }
