a8264a2aa80ce5ad1a06c22bf9128b3cdc2d9000
Fernando <fernandochaza@gmail.com>
2023-05-05 08:38:56 -0300

    fix: Abstract options and header into the Menu
    Now the options are defined inside the Menu class and the DisplayHeader
    was changed to ReturnHeader and now gets the username as an argument and
    returns a custome message

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -11,20 +11,9 @@ public class Program
         Journal journal = new Journal();
         journal._name = "John";
 
-        // Define a header
-        string header = $"Hello {journal._name}! Welcome to your Journal Manager";
-
-        // Create the menu options
-        List<string> options = new List<string>();
-        options.Add("Create a new entry");
-        options.Add("Display my journal");
-        options.Add("Save today's entries");
-        options.Add("Load my journal");
-        options.Add("Quit");
-
         // Instantiate a new Menu
-        Menu mainMenu = new Menu(header, options);
-        mainMenu.DisplayHeader();
+        Menu mainMenu = new Menu();
+        mainMenu.ReturnHeader(journal._name);
 
         do
         {
