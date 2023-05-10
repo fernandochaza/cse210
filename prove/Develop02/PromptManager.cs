b54b3da71a184142fdc321eca0d15acddfa3a565
Fernando <fernandochaza@gmail.com>
2023-05-09 23:33:04 -0300

    feat: Refactor and improve Prompt generation
    Prompt class now is called PromptManager and has aditional features
    - The prompts are stored in a txt file
    - The application loads the file at runtime
    - The user will have an option to add custom prompts that will be stored
      in the prompts database
    - A method to display all the prompts

---
+++ prove/Develop02/PromptManager.cs
@@ -0,0 +1,50 @@
+/// <summary> Manage the application prompts
+/// <list type="bullet">
+/// <item>
+/// </item>
+/// <item>
+/// Load prompts from a txt file
+/// </item>
+/// <item>
+/// Save user's custom prompts in the file
+/// </item>
+/// <item>
+/// Generate a random prompt
+/// </item>
+/// </list>
+/// </summary>
+public class PromptManager
+{
+    public List<string> _prompts;
+
+    public PromptManager()
+    {
+        _prompts = new List<string>();
+
+        // Load prompts from a txt file
+        string[] lines = System.IO.File.ReadAllLines("prompts.txt");
+        foreach (string line in lines)
+        {
+            _prompts.Add(line);
+        }
+    }
+
+    /// <summary>
+    /// Select a random prompt from a predefined array of prompts
+    /// </summary>
+    /// <returns>A random prompt</returns>
+    public string ReturnRandomPrompt()
+    {
+        Random random = new Random();
+        int index = random.Next(_prompts.Count);
+        return _prompts[index];
+    }
+
+    /// <summary>
+    /// Add a custom prompt to the prompts file
+    /// </summary>
+    public void AddCustomPrompt()
+    {
+
+    }
+}
