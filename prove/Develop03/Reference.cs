b6a6248609aa071accc2916aee6fed91467a0c3d
Fernando <fernandochaza@gmail.com>
2023-05-16 16:04:39 -0300

    feat: Completed class constructors and methods
    A constructor that takes 1 parameter for raw scriptures
    A constructor that takes 3 parameters (book, chapter, initial verse)
    A constructor that takes 4 parameters (book, chapter, initial verse,
    ending verse)
    
    A method that overrises the ToString method to return a string
    representation of a Reference

--- prove/Develop03/Reference.cs
+++ prove/Develop03/Reference.cs
@@ -7,4 +7,109 @@ public class Reference
     private int _chapter;
     private int _initialVerse;
     private int _endingVerse;
+
+    /// <summary>
+    /// Instantiate a new Reference from a raw string reference formatted as "Book 1:2-3"
+    /// where the ending verse is optional
+    /// </summary>
+    public Reference(string rawReference)
+    {
+        // Define the string separators
+        string bookAndPassageSeparator = " ";
+        string chapterAndVerseSeparator = ":";
+        string versesSeparator = "-";
+        int endingVerse = 0;
+        int initialVerse = 0;
+
+        // Divide the reference into Book and Passage
+        string[] referenceParts = rawReference.Split(bookAndPassageSeparator);
+        string book = referenceParts[0];
+        
+        // Divide the passage into Chapter and Verse/s
+        string[] passage = referenceParts[1].Split(chapterAndVerseSeparator);
+        int chapter = int.Parse(passage[0]);
+
+        // Check if the passage contains more than a single verse
+        if (passage[1].Contains(versesSeparator))
+        {
+            // Divide initial and ending verses
+            string[] verses = passage[1].Split(versesSeparator);
+            initialVerse = int.Parse(verses[0]);
+            endingVerse = int.Parse(verses[1]);
+        }
+        else
+        {
+            initialVerse = int.Parse(passage[1]);
+        }
+
+        _book = book;
+        _chapter = chapter;
+        _initialVerse = initialVerse;
+
+        if (endingVerse != 0)
+        {
+            _endingVerse = endingVerse;
+        }
+    }
+
+    /// <summary>
+    /// Instantiate a new Reference given the book name, chapter, and single verse
+    /// </summary>
+    public Reference(string book, int chapter, int InitialVerse)
+    {
+        _book = "book";
+        _chapter = chapter;
+        _initialVerse = InitialVerse;
+    }
+
+
+    /// <summary>
+    /// Instantiate a new Reference given the book name, chapter, initial verse, and ending verse
+    /// </summary>    
+    public Reference(string book, int chapter, int InitialVerse, int endingVerse)
+    {
+        _book = "book";
+        _chapter = chapter;
+        _initialVerse = InitialVerse;
+        _endingVerse = endingVerse;
+    }
+
+    /// <summary>
+    /// Return a string representation of the Reference
+    /// </summary>
+    public override string ToString()
+    {
+        string referenceString = $"{_book} {_chapter}:{_initialVerse}";
+
+        if (_endingVerse != 0)
+        {
+            referenceString += $"-{_endingVerse}";
+        }
+
+        return referenceString;
+    }
+
+
+    // public string Book 
+    // {
+    //     get {return _book;}
+    // }
+
+
+    // public int Chapter 
+    // {
+    //     get {return _chapter;}
+    // }
+
+
+    // public int InitialVerse 
+    // {
+    //     get {return _initialVerse;}
+    // }
+
+
+    // public int EndingVerse 
+    // {
+    //     get {return _endingVerse;}
+    // }
 }
