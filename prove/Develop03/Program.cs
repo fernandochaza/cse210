0023747d5abdb30a1d62c1664e7ae8b6c522ff6c
Fernando <fernandochaza@gmail.com>
2023-05-16 17:36:36 -0300

    feat: Added new options habdling
    handle display Scriptures options
    handle Add new scripture option

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,11 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Instantiate a new Scriptures database
+        MyScriptures myScriptures = new MyScriptures();
+        myScriptures.Initialize();
+
         // Instantiate a new Menu
         Menu mainMenu = new Menu();
 
         // Display welcome message
         mainMenu.DisplayWelcome("Fernando");
+
+        // Variable to store the user option choice
+        string selectedOption;
+        
+        do
+        {
+            // Display the menu options
+            mainMenu.DisplayOptions();
+
+            // Read the user choice
+            selectedOption = Console.ReadLine();
+
+            // Handle each menu option
+            switch (selectedOption)
+            {
+                case "1":
+                    myScriptures.DisplayScriptures();
+                    break;
+                case "2":
+                    Scripture newScripture = new Scripture();
+                    myScriptures.AddScriptureToDatabase(newScripture);
+                    break;
+                case "3":
+                    break;
+            }
+        } while (selectedOption != "4");
 
     }
 }
