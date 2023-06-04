320164afba543356a20833999249062c81acf435
Fernando Chazarreta <fernandochaza@gmail.com>
2023-06-04 13:44:45 -0300

    feat: Finish preparing activity. Inheritance

--- prepare/Learning04/Assignment.cs
+++ prepare/Learning04/Assignment.cs
@@ -13,4 +13,9 @@ public class Assignment
     {
         return $"{_studentName} - {_topic}";
     }
+
+    public string StudentName
+    {
+        get { return _studentName;}
+    }   
 }
--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -6,8 +6,20 @@ class Program
     {
         string name = "Fernando Chazarreta";
         string topic = "Inheritance";
+        string section = "9.8";
+        string problems = "1-10";
+        string title = "Alice in wonderland";
 
         Assignment assignment = new Assignment(name, topic);
         Console.WriteLine(assignment.GetSummary());
+
+        MathAssignment mathAssignment = new MathAssignment(name, topic, section, problems);
+
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        WritingAssignment writingAssignment = new WritingAssignment(name, topic, title);
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
