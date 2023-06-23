acfca872a3aa883523f44b6707bd07573c8e9bfd
Fernando <fernandochaza@gmail.com>
2023-06-23 15:28:41 -0300

    feat: Add switch to handle menu options

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -4,6 +4,50 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop05 World!");
+        Console.Clear();
+
+        // Instantiate a new Menu
+        Menu mainMenu = new Menu();
+
+        // Display welcome message
+        mainMenu.DisplayWelcome("Fernando");
+
+        // Variable to store the user option choice
+        string selectedOption;
+        
+        do
+        {
+            // Display the menu options
+            mainMenu.DisplayOptions();
+
+            // Read the user choice
+            selectedOption = Console.ReadLine();
+
+            // Handle each menu option
+            switch (selectedOption)
+            {
+                case "1":
+
+                    break;
+                case "2":
+
+                    break;
+                case "3":
+
+                    break;
+                case "4":
+
+                    break;
+                case "5":
+
+                    break;
+                case "6":
+
+                    break;
+                default:
+                    Console.WriteLine("\n(!) Please select a valid option: ");
+                    break;
+            }
+        } while (selectedOption != "6");
     }
 }
