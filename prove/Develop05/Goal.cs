9b728dbf9f9372a42f474e83603347ba7bf1d8ae
Fernando <fernandochaza@gmail.com>
2023-06-23 19:43:52 -0300

    feat: Create Goal superclass structure, member var and methods

---
+++ prove/Develop05/Goal.cs
@@ -0,0 +1,30 @@
+public abstract class Goal
+{
+    private string _type;
+    private string _shortDescription;
+    private string _longDescription;
+    private int _points;
+    private bool _isCompleted;
+
+    public Goal()
+    {
+
+    }
+
+    public void CreateGoal()
+    {
+
+    }
+
+    public void MarkCompleted()
+    {
+
+    }
+
+    public void DisplayGoal()
+    {
+
+    }
+
+    public abstract string GetStringRepresentation();
+}
